ed458218f4b7e0efaa4dc5967fe517c019bf3093
robinj97 <35749463+robinj97@users.noreply.github.com>
2022-04-30 17:06:05 +0200

    Started initial structure
    Created a JSON schema to contain questions and answers. Need to figure out how to loop over answers.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
   <h1>Are you a true One Piece Fan?</h1>
+  Need Question component here
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import  BaseButton from "./components/BaseButton.vue;
+import quiz from "./quiz.json.json";
+export default defineComponent({
+  components: { BaseButton },
+  setup() {
+
+  }
+});
+</script>
+
---
+++ src/components/Question.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <h1>{{ questionText }}</h1>
+  <BaseButton v-for="(question, answer) in questionsList" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BaseButton from "./BaseButton.vue";
+import { Questions } from "src/quiz.json";
+export default defineComponent({
+  components: { BaseButton },
+  props: {
+    questionText: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const questionsList = Questions;
+    const questionTextProp = props.questionText;
+    return { questionTextProp, questionsList };
+  },
+});
+</script>
